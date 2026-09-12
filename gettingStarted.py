188c34347165f3d7e9597ab94b79fc3daaa28ac0
iblazey <iblazey@iu.edu>
2026-09-12 16:30:38 -0400

    Update assignment answers

--- gettingStarted.py
+++ gettingStarted.py
@@ -18,9 +18,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = int(2)
+        answer = 5
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = int(3)
+        answer = 3
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "pcap"
     else: 
